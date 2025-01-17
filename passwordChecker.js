07b2447e22dbeecc701c5d11a2c26ba6201fc616
Mike Pourbaix <mike.pourbaix@student.vinci.be>
2025-01-17 16:50:27 +0100

    add: digitalPattern in function isValidPassword and its tests

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,6 +1,7 @@
 function isValidPassword(password) {
     const minLength = 8;
     const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/;
+    const digitalPattern = /[0-9]/;
 
 
     if (password.length < minLength) {
@@ -9,8 +10,12 @@ function isValidPassword(password) {
     if(!password.match(specialCharPattern)){
         return false;
     }
+    if(!password.match(digitalPattern)){
+        return false;
+    }
     
     return true;
 };
 
 module.exports = { isValidPassword };
+ 
--- tests/passwordChecker.test.js
+++ tests/passwordChecker.test.js
@@ -3,13 +3,18 @@ const { isValidPassword } = require('../passwordChecker');
 describe("Password Checker, check if the password pass several restrictions", () => {
 
     it("should return false if the password is shorter than 8 characters", () => {
-        expect(isValidPassword('short?')).toBe(false);
-        expect(isValidPassword('shortButNotTooShort!')).toBe(true);
+        expect(isValidPassword('0short?')).toBe(false);
+        expect(isValidPassword('0shortButNotTooShort!')).toBe(true);
     });
 
     it("should return false if the password does not contain a special character", () => {
-        expect(isValidPassword('noSpecialCharacter')).toBe(false);
-        expect(isValidPassword('Spec!')).toBe(false);
-        expect(isValidPassword('SpecialCharacter!')).toBe(true);
+        expect(isValidPassword('1noSpecialCharacter')).toBe(false);
+        expect(isValidPassword('1Spec!')).toBe(false);
+        expect(isValidPassword('1SpecialCharacter!')).toBe(true);
+    });
+
+    it("should return false if the password does not contain a digit", () => {
+        expect(isValidPassword('noDigit?')).toBe(false);
+        expect(isValidPassword('Digit1!!')).toBe(true);
     });
 });
