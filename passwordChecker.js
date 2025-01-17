aa74aa77ff64d628790ab8d4de49729ce80cc397
Mike Pourbaix <mike.pourbaix@student.vinci.be>
2025-01-17 16:46:45 +0100

    add: specialCharPattern restriction in function isValidPassword and its test, and refactoring first tests to match the new restriction

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,7 +1,12 @@
 function isValidPassword(password) {
     const minLength = 8;
+    const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/;
+
 
     if (password.length < minLength) {
+        return false;
+    }
+    if(!password.match(specialCharPattern)){
         return false;
     }
     
--- tests/passwordChecker.test.js
+++ tests/passwordChecker.test.js
@@ -1,9 +1,15 @@
 const { isValidPassword } = require('../passwordChecker');
 
-describe("Password Checker check if the password is good", () => {
+describe("Password Checker, check if the password pass several restrictions", () => {
 
     it("should return false if the password is shorter than 8 characters", () => {
-        expect(isValidPassword('short')).toBe(false);
-        expect(isValidPassword('shortButNotTooShort')).toBe(true);
+        expect(isValidPassword('short?')).toBe(false);
+        expect(isValidPassword('shortButNotTooShort!')).toBe(true);
+    });
+
+    it("should return false if the password does not contain a special character", () => {
+        expect(isValidPassword('noSpecialCharacter')).toBe(false);
+        expect(isValidPassword('Spec!')).toBe(false);
+        expect(isValidPassword('SpecialCharacter!')).toBe(true);
     });
 });
